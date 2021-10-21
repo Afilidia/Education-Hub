8c239d561361425b2f494facdb84aa06b8d43e96
wikgu <48187208+wikgu@users.noreply.github.com>
2021-10-21 04:29:28 +0200

    Update server.js

--- routes/server.js
+++ routes/server.js
@@ -18,7 +18,7 @@ host.style('/buttons.css', cache.styles.buttons, ()=>{
 
 //		Landing page	 //
 host.pager("", "landing/", ()=>{return true}, "/");
-host.pager("", "login/", (req, res)=>{if(req.cookies.token&&require("../cookies.json")[req.cookies.token]) return false; else true;}, "/app");
+host.pager("", "login/", (req, res)=>{if(req.cookies.token&&require("../cookies.json")[req.cookies.token]) return false; else return true;}, "/app");
 host.pager("/app", "app/", (req, res)=>{if(req.cookies.token&&require("../cookies.json")[req.cookies.token]) return true;}, "/login");
 
 module.exports = router;
