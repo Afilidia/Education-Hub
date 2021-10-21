b0c8c7d3aa49ef035430020efba429c4d5e3c07d
wikgu <48187208+wikgu@users.noreply.github.com>
2021-10-21 04:31:45 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -226,8 +226,12 @@ router.post('/register', async (req, res, next) => {
       do {
           r = getRandomString(40);
       } while(tokens[r]);
-      tokens[r] = {user: q2[0].id, created: new Date().getTime(), lastActivity: new Date().getTime(), ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress};
+      let data = q[0];
+      data.password = "";
+      data = JSON.stringify(data);
+      tokens[r] = {user: q2[0].id, data, created: new Date().getTime(), lastActivity: new Date().getTime(), ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress};
       res.cookie('token', r);
+      res.cookie('data', data);
       res.redirect("/app");
   } else res.redirect('/register?error=1');
 });
