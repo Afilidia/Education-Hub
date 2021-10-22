f5e149095a3d974e47d3fc5ceeaca94bddd0e987
wikgu <wikgu@op.pl>
2021-10-22 03:22:47 +0200

    Update logo.js

--- framework/logo.js
+++ framework/logo.js
@@ -1,5 +1,11 @@
 module.exports = ``+//$(bg-blue)
 `
+$(gb-reset)$(fg-green)    ______    __      __  ____  ______ 
+$(gb-reset)$(fg-green)   / ____/___/ /_  __/ / / / / / / __ )
+$(gb-reset)$(fg-green)  / __/ / __  / / / / /_/ / / / / __  |
+$(gb-reset)$(fg-green) / /___/ /_/ / /_/ / __  / /_/ / /_/ / 
+$(gb-reset)$(fg-green)/_____/\__,_/\__,_/_/ /_/\____/_____/ `;
+/*`
 $(gb-reset)$(fg-green)                ooooooooooooo
 $(gb-reset)$(fg-green)              ooo    ooooooooooo    $(fg-yellow)$(gb-bright)|)    |-    
 $(gb-reset)$(fg-green)              ooo    ooooooooooo    $(fg-yellow)$(gb-bright)|\\(||)|_()|\`
@@ -16,4 +22,4 @@ $(gb-reset)$(fg-green)        ooooooooooo
 $(gb-reset)$(fg-green)        ooooo ooooo
 $(gb-reset)$(fg-green)        oooo   oooo
 $(gb-reset)$(fg-green)        ooo     ooo
-$(gb-reset)$(fg-green)        oooo    oooo`;
+$(gb-reset)$(fg-green)        oooo    oooo`;*/
