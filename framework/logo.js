16baec7936ed0620b96aee389cb644bae16da85c
wikgu <wikgu@op.pl>
2021-10-22 03:24:02 +0200

    Update logo.js

--- framework/logo.js
+++ framework/logo.js
@@ -4,7 +4,8 @@ $(gb-reset)$(fg-green)    ______    __      __  ____  ______
 $(gb-reset)$(fg-green)   / ____/___/ /_  __/ / / / / / / __ )
 $(gb-reset)$(fg-green)  / __/ / __  / / / / /_/ / / / / __  |
 $(gb-reset)$(fg-green) / /___/ /_/ / /_/ / __  / /_/ / /_/ / 
-$(gb-reset)$(fg-green)/_____/\__,_/\__,_/_/ /_/\____/_____/ `;
+$(gb-reset)$(fg-green)/_____/\\__,_/\\__,_/_/ /_/\\____/_____/ 
+`;
 /*`
 $(gb-reset)$(fg-green)                ooooooooooooo
 $(gb-reset)$(fg-green)              ooo    ooooooooooo    $(fg-yellow)$(gb-bright)|)    |-    
