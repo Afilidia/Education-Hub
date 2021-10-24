caa0e62c77e1515e3834c33f2c30877a617250f9
Adrian <adrian_iskra@o2.pl>
2021-10-24 18:26:57 +0200

    Compiler upgrade (snippets, saving)

--- public/js/compiler.js
+++ public/js/compiler.js
@@ -9,16 +9,28 @@ $(document).ready(async function () {
     var currentLanguage = 'js';
     var run = document.getElementById('run');
     var code = getStartingCode(currentLanguage);
+    var filename = document.getElementById('filename');
 
     var options = document.getElementById('app-cover').querySelectorAll('.option');
+    var buttons = document.querySelectorAll('.editor-btn');
 
     var theme = true;
 
     const modes = {
-        js: ace.require("ace/mode/javascript").Mode,
-        py: ace.require("ace/mode/python").Mode, 
-        cs: ace.require("ace/mode/csharp").Mode
+        js: "ace/mode/javascript",
+        py: "ace/mode/python",
+        cs: "ace/mode/csharp",
+        java: "ace/mode/java",
+        php: "ace/mode/php",
     };
+
+    const files = {
+        js: 'app.js',
+        py: 'main.py',
+        cs: 'main.cs',
+        java: 'main.jar',
+        php: 'index.php'
+    }
 
 
     // * Plug input & output to the lib functions
@@ -26,19 +38,19 @@ $(document).ready(async function () {
 
     const ACE = {
         init: function() {
-            editor.session.setMode(new modes.js());
+            editor.session.setMode(getMode());
 
             let cookieTheme = getThemeFromCookies();
 
             if (cookieTheme) {
                 editor.setTheme(cookieTheme);
+
                 if (cookieTheme.endsWith('dracula')) {
                     document.body.setAttribute('data-theme', theme);
                     theme = !theme;
                 }
 
-            }
-            else {
+            } else {
                 editor.setTheme('ace/theme/eclipse');
                 setThemeInCookies('ace/theme/eclipse');
             }
@@ -54,13 +66,46 @@ $(document).ready(async function () {
 
     ACE.init();
 
-    var buttons = document.querySelectorAll('.editor-btn');
+    // * Select saved language
+    if (getLanguageFromCookies()) {
+        var lang = getLanguageFromCookies();
+
+        var option = (() => {
+            let o = null;
+            if (options) options.forEach(opt => {
+                if (opt.getAttribute('data-language') == lang) o = opt;
+            });
+
+            return o;
+        })();
+
+        let radios = option.querySelectorAll('input[type="radio"]');
+
+        if (radios) radios.forEach(radio => {
+            radio.checked = true;
+        });
+
+        prepareEditorForLanguage(lang);
+    }
 
     if (buttons) buttons.forEach(button => {
         button.addEventListener('click', () => {
             let type = button.getAttribute('data-type');
             switch (type) {
-                case 'download': {} break;
+                case 'download': {
+                    // * Download current file
+                    var blob = new Blob(['' + getCode()], {type: 'text/plain;charset=utf-8'});
+                    var url = window.URL || window.webkitURL;
+                    var link = url.createObjectURL(blob);
+                    var a = document.createElement('a');
+
+                    a.download = files[currentLanguage];
+                    a.href = link;
+
+                    document.body.appendChild(a);
+                    a.click();
+                    document.body.removeChild(a);
+                } break;
 
                 case 'fullscreen': {
                     var editor = document.querySelector('.editor');
@@ -80,15 +125,17 @@ $(document).ready(async function () {
 
     if (options) options.forEach(option => {
         option.addEventListener('click', () => {
-            let lang = option.getAttribute('data-language');
-            currentLanguage = lang;
-            editor.session.setMode(new modes[currentLanguage]());
+            // * Save the latest code in cookies
+            // saveCodeToCookies(currentLanguage);
 
-            // ! Temporary set the starting code when switched to 
-            // ! The other language
-            // ! -> Later save the code before switching and load it
-            // ! -> when switched back
-            editor.setValue(getStartingCode(lang));
+            // * Switch to clicked
+            let lang = option.getAttribute('data-language');
+            let input = document.getElementById('options-view-button');
+            currentLanguage = lang;
+
+            setLanguageInCookies(lang);
+            prepareEditorForLanguage(lang);
+            input.checked = false;
         });
     });
 
@@ -98,12 +145,44 @@ $(document).ready(async function () {
         // * Send to server
     });
 
+
+
     function getTheme() {
         return (!theme) ? 'ace/theme/dracula' : 'ace/theme/eclipse';
     }
 
+    function getMode(lang) {
+        return modes[lang] || modes['js'];
+    }
+
     function getStartingCode(lang) {
         return startingLanguageCode[lang];
+    }
+
+    function prepareEditorForLanguage(lang) {
+        currentLanguage = lang;
+        editor.session.setMode(getMode(currentLanguage));
+        if (filename) filename.textContent = files[currentLanguage];
+
+        // ! Temporary set the starting code when switched to
+        // ! The other language
+        // ! -> Later save the code before switching and load it
+        // ! -> when switched back
+        editor.setValue(getStartingCode(currentLanguage));
+    }
+
+    // * Gets value from editor
+    function getCode() {
+        return editor.getValue();
+    }
+
+    function saveCodeToCookies(lang) {
+        code[lang] = getCode();
+        $.cookies(_USER_CODE, JSON.stringify(code), {path: '/', expires: 365});
+    }
+
+    function getCodeFromCookies() {
+        return JSON.parse($.cookies(_USER_CODE)) || '';
     }
 
     // * Cookies functions
@@ -113,5 +192,13 @@ $(document).ready(async function () {
 
     function getThemeFromCookies() {
         return $.cookie(_ACE_THEME);
+    }
+
+    function setLanguageInCookies(lang) {
+        $.cookie(_ACE_LANGUAGE_SELECTED, lang, {path: '/', expires: 1});
+    }
+
+    function getLanguageFromCookies() {
+        return $.cookie(_ACE_LANGUAGE_SELECTED);
     }
 });
--- public/js/config.js
+++ public/js/config.js
@@ -13,6 +13,8 @@ const _FILE_STATE = '_file-state';
 const _LOCAL_SUM_HISTORY = '_sum_history';
 
 const _ACE_THEME = '_ace-theme';
+const _ACE_LANGUAGE_SELECTED = '_ace-selected-l';
+const _USER_CODE = '_user-code';
 
 const _COOKIES_ACCEPTED = '_cookies_accepted';
 
@@ -26,10 +28,20 @@ const ENDPOINTS = {
     }
 };
 
+const code = {
+    js: '',
+    py: '',
+    cs: '',
+    java: '',
+    php: ''
+};
+
 const startingLanguageCode = {
-    js: '// Type your code here...',
-    py: '# Type your code here...',
-    cs: '// Type your code here...'
+    js: `// This is a automatically generated Javascript file\n// Click the 'Run' button on the right to see the output\n\n(function () {\n\tconsole.log('Hello world!');\n})();`,
+    py: `# This is a automatically generated Python file\n# Click the 'Run' button on the right to see the output\n\nif __name__ == '__main__':\n\tprint("Hello world!")\n`,
+    cs: `// This is a automatically generated C# file\n// Click the 'Run' button on the right to see the output\n\nusing System;\n\npublic class Class {\n\n\tpublic Class() {\n\t\n\t}\n\n\tstatic void Main(string[] args) {\n\t\n\t}\n}`,
+    java: `// This is a automatically generated Java file\n// Click the 'Run' button on the right to see the output\n\npublic class Class {\n\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello world!");\n\t}\n}`,
+    php: `<?php\n\t// This is a automatically generated PHP file\n// Click the 'Run' button on the right to see the output\n\necho('Hello world!');\n\t`,
 };
 
 // * FILE * //
